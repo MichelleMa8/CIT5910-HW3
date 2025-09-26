eef71c23f812d497417b0630c0ac33fb10f6dec8
Michelle <qianranma8@gmail.com>
2025-09-25 21:10:57 -0400

    add isNarcissistic function

--- src/PositiveInteger.java
+++ src/PositiveInteger.java
@@ -6,46 +6,45 @@ public class PositiveInteger {
         num = number;
     }
 
+    private int sumOfProperDivisors(int n) {
+        int sum = 1;
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0) {
+                sum += i;
+                int other = n / i;
+                if (other != i) sum += other;
+            }
+        }
+        return sum;
+    }
+
     public boolean isPerfect() {
         if (num <= 1){
             return false;
         }
-
-        int sumFactor = 1;
-        double numSqrt = (int) Math.sqrt(num);
-        for (int i = 2; i <= numSqrt; i++){
-            if (num % i == 0){
-                sumFactor += i;
-                int other = num / i;
-                if (other != i){
-                    sumFactor += other;
-                }
-            }
-        }
-        return sumFactor == num;
+        return sumOfProperDivisors(num) == num;
     }
 
     public boolean isAbundant() {
         if (num <= 1){
             return false;
         }
-
-        int sumFactor = 1;
-        double numSqrt = (int) Math.sqrt(num);
-        for (int i = 2; i <= numSqrt; i++){
-            if (num % i == 0){
-                sumFactor += i;
-                int other = num / i;
-                if (other != i){
-                    sumFactor += other;
-                }
-            }
-        }
-        return sumFactor > num;
+        return sumOfProperDivisors(num) > num;
     }
 
     public boolean isNarcissistic() {
-        return false;
-    }
+        if (num >= 0 && num < 10){
+            return true;
+        }
 
+        int sum = 0;
+        int tmp = num;
+        while (tmp > 0){
+            int lastDigit = tmp % 10;
+            sum += lastDigit * lastDigit;
+            tmp /= 10;
+        }
+        return sum == num;
+    }
 }
