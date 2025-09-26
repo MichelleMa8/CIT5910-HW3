1555df25d6aa93bdf6a75a5132147c3aa9cce1e0
Michelle <qianranma8@gmail.com>
2025-09-25 20:45:46 -0400

    add isAbundant function

--- src/PositiveInteger.java
+++ src/PositiveInteger.java
@@ -26,7 +26,22 @@ public class PositiveInteger {
     }
 
     public boolean isAbundant() {
-        return false;
+        if (num <= 1){
+            return false;
+        }
+
+        int sumFactor = 1;
+        double numSqrt = (int) Math.sqrt(num);
+        for (int i = 2; i <= numSqrt; i++){
+            if (num % i == 0){
+                sumFactor += i;
+                int other = num / i;
+                if (other != i){
+                    sumFactor += other;
+                }
+            }
+        }
+        return sumFactor > num;
     }
 
     public boolean isNarcissistic() {
